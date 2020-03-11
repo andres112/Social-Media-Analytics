42d7ca68502b0df3682d75a240e82d8aa5870028
Andres Felipe Dorado <andres.dorado90@gmail.com>
2020-03-11 16:04:42 +0100

    Third exercise for SMA about communities detection - CPM algorithm implementation with networkx

---
+++ Third Ex/cliques.py
@@ -0,0 +1,34 @@
+from itertools import combinations
+import networkx as nx
+
+
+def k_cliques(graph):
+    # 2-cliques
+    cliques = [{i, j} for i, j in graph.edges() if i != j]
+    k = 2
+
+    while cliques:
+        # result
+        yield k, cliques
+
+        # merge k-cliques into (k+1)-cliques
+        cliques_1 = set()
+        for u, v in combinations(cliques, 2):
+            w = u ^ v
+            if len(w) == 2 and graph.has_edge(*w):
+                cliques_1.add(tuple(u | w))
+
+        # remove duplicates
+        cliques = list(map(set, cliques_1))
+        k += 1
+
+
+def print_cliques(graph, size_k):
+    for k, cliques in k_cliques(graph):
+        if k == size_k:
+            print('%d-cliques = %d, %s.' % (k, len(cliques), cliques))
+
+
+size_k = 3
+graph = nx.read_edgelist('DataSets/got.txt')
+print_cliques(graph, size_k)
---
+++ Third Ex/cpm.py
@@ -0,0 +1,56 @@
+import csv
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+class Graph:
+    def __init__(self):
+        self.G = nx.Graph()
+
+    def createGraph(self, data):
+        for item in data:
+            self.G.add_edge(item[0], item[1], weight=item[3])
+
+    def getGraphInfo(self):
+        return nx.info(self.G)
+
+    def __getCliques(self):
+        return sorted(list(nx.enumerate_all_cliques(self.G)), key=len)
+
+    def drawGraph(self):
+        # Drawing the Graph using matplotlib
+        nx.draw_networkx(self.G, node_color=['red'], with_labels=True)
+        plt.show()
+
+    def numberOfCliques(self):
+        nq = nx.graph_number_of_cliques(self.G)  # Number of cliques in graph
+        bq = nx.graph_clique_number(self.G)  # Max clique size of Graph
+        return nq, bq
+
+    def numberOfCliquesByK(self, k):
+        # get all the cliques with
+        cliques = [x for x in self.__getCliques() if len(x) == k]
+        return cliques
+
+
+with open('DataSets/book1.csv', 'r') as dataset:  # Open the file
+    nodereader = csv.reader(dataset)  # Read the csv
+    # Retrieve the data (using Python list comprhension and list slicing to remove the header row, see footnote 3)
+    rows = [n for n in nodereader][1:]
+
+if __name__ == "__main__":
+    community = Graph()
+    community.createGraph(rows)
+    print(community.getGraphInfo())
+    print('Number of Cliques: {}\nMax Size Clique: {}\n***'.format(
+        community.numberOfCliques()[0], community.numberOfCliques()[1]))
+    # define the value of k, can be modified 
+    k = 3
+    cliquesByK = community.numberOfCliquesByK(k)
+    print('Number of cliques with k = {}: {}\n***'.format(k, len(cliquesByK)))
+
+    # for the max size clique in graph
+    k = community.numberOfCliques()[1]
+    cliquesByK = community.numberOfCliquesByK(k)
+    print('Number of cliques with k = {}: {}\n***'.format(k, len(cliquesByK)))
+    print(cliquesByK)
