e6daa817e9602a225ac2a7a737a0a2bcb929c999
Andres Dorado <andres.dorado90@gmail.com>
2020-05-02 15:36:39 +0200

    Include dataset full nan values
    include in split Data function the posibility to fill the nan values with zeros or mean value

--- Project/splitData.py
+++ Project/splitData.py
@@ -4,11 +4,18 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-def split_train_test(data, percent_test=0.1):
+def split_train_test(data, user_means, fill_with='nan', percent_test=0.1):
+    user_means = user_means.to_dict()['movieRating']
     train, test = train_test_split(data, test_size= percent_test)
 
     train = train.pivot_table(index='movieId', columns="userId", values="movieRating")    
-    # train = train.fillna(train.mean(axis=0, skipna=True))
     test = test.pivot_table(index='movieId', columns="userId", values="movieRating")
+    
+    if(fill_with == 'mean'):
+        train = train.fillna(user_means)    
+        test = test.fillna(user_means)
+    if(fill_with == 'zeros'):
+        train = train.fillna(0)    
+        test = test.fillna(0)
     
     return train, test
